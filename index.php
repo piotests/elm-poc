9c5819124c5a8ca960579c4b30045f491e5b0cf5
testim <itayf@macbook-pro.home>
2020-10-26 16:27:29 +0200

    Add index file

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-<!--    <meta name="csrf-token" content="--><?php //echo $_SESSION['token']; ?><!--">-->
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
     <title>Title</title>
     <link rel="stylesheet" type="text/css" href="assets/css/app.css" />
